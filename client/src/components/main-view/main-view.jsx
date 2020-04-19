a5c0b001bc69364fba246718f4af493a398a30ef
Jacqueline Purification <53584408+jtp2019@users.noreply.github.com>
2020-04-19 02:50:24 +0200

    Added login and regi com plus bootstap

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
 /*sources of Bootstrap code*/
 import Container from "react-bootstrap/Container";
@@ -14,55 +14,58 @@ import { RegistrationView } from "../registration-view/registration-view";
 
 export class MainView extends Component {
   constructor(props) {
- /* Call the superclass constructor so React can initialize it */
+    /* Call the superclass constructor so React can initialize it */
     super(props);
-/* Initialize the state to an empty object so we can destructure it later */
+    /* Initialize the state to an empty object so we can destructure it later */
     this.state = {
       movies: null,
       selectedMovie: null,
       user: null,
-      register: false
+      register: false,
     };
   }
 
   componentDidMount() {
-    const endpoint = 'https://rhubarb-crisp-92657.herokuapp.com/movies';
-    axios.get(endpoint)
-      .then(res => {
+    const endpoint = "https://rhubarb-crisp-92657.herokuapp.com/movies";
+    axios
+      .get(endpoint)
+      .then((res) => {
         console.log(res.data[0]);
-        this.setState({ movies: res.data })
+        this.setState({ movies: res.data });
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
   }
 
-/*clicking movie to get more info*/
+  /*clicking movie to get more info*/
   onMovieClick(movie) {
     this.setState({
-      selectedMovie: movie
+      selectedMovie: movie,
     });
   }
 
   onLoggedIn(user) {
     this.setState({
-      user
+      user,
     });
   }
 
-  onRegister () {
-      if(!this.state.register){
-        this.setState({register: true});
-      }else{
-        this.setState({register: false});
-      }
-    };
+  onRegister() {
+    if (!this.state.register) {
+      this.setState({ register: true });
+    } else {
+      this.setState({ register: false });
+    }
+  }
 
-   /* This overrides the render() method of the superclass, No need to call super() though, as it does nothing by default */
+  /* This overrides the render() method of the superclass, No need to call super() though, as it does nothing by default */
   render() {
     /* if the state isn't initialized, this will throw on runtime, before the data is initially loaded */
     const { movies, selectedMovie, user, register } = this.state;
-    if (!user && !register) return <LoginView onClick={this.onRegister} onLoggedIn={user => this.onLoggedIn(user)} />
-    if (register) return <RegistrationView  onClick={ this.onRegister.bind(this) }  />
-  /* before the movies have been loaded */
+    if (!user)
+      return <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />;
+    if (register)
+      return <RegistrationView onClick={this.onRegister.bind(this)} />;
+    /* before the movies have been loaded */
     if (!movies) return <div className="main-view" />;
 
     return (
@@ -70,12 +73,19 @@ export class MainView extends Component {
         <Container>
           <Row>
             {selectedMovie ? (
-              <MovieView movie={selectedMovie} previous={movie => this.onMovieClick(!movie)} />
+              <MovieView
+                movie={selectedMovie}
+                previous={(movie) => this.onMovieClick(!movie)}
+              />
             ) : (
-                movies.map(movie => (
-                  <Col key={movie._id} xs={12} sm={6} md={4}>
-                  <MovieCard key={movie._id} movie={movie} click={movie => this.onMovieClick(movie)} />
-                   </Col>
+              movies.map((movie) => (
+                <Col key={movie._id} xs={12} sm={6} md={4}>
+                  <MovieCard
+                    key={movie._id}
+                    movie={movie}
+                    click={(movie) => this.onMovieClick(movie)}
+                  />
+                </Col>
               ))
             )}
           </Row>
